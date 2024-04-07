035b608d9b3ab4da3718315bf2823db59c2e008a
PooyaAbbasi <poy.8383.abs@gmail.com>
2024-04-07 22:37:44 +0330

    create, register, and migrate Booking and Menu models of restaurant.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20, blank=True)
+    guest_number = models.IntegerField()
+    comment = models.CharField(max_length=1000, blank=True)
+    last_modification_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'guest name:{self.first_name} {self.last_name} \n' + \
+                f' guest number:{self.guest_number} -- last update time:{self.last_modification_time}'
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField(null=True)
+    menu_item_description = models.TextField(max_length=1000, default='', blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
