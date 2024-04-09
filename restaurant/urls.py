ddd8feda6e0af60f8dadefbc8d68825953e0678f
PooyaAbbasi <poy.8383.abs@gmail.com>
2024-04-09 13:49:21 +0330

    Create menu and menu details page

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,5 +4,8 @@ from .views import *
 app_name = 'restaurant'
 
 urlpatterns = [
-    path('home/', home_page, name='home-page')
+    path('home/', home_page, name='home-page'),
+    path('menu/', MenuList.as_view(), name='menu-list'),
+    path('menu/<int:pk>/', MenuDetail.as_view(), name='menu-detail'),
+
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
 
@@ -6,3 +9,16 @@ from django.shortcuts import render
 def home_page(request):
     context = {}
     return render(request, 'restaurant/home-page.html', context)
+
+
+class MenuList(ListView):
+    model = Menu
+    template_name = 'restaurant/menu.html'
+    template_name_suffix = ''
+    ListView.context_object_name = 'menu_items'
+
+
+class MenuDetail(DetailView):
+    model = Menu
+    template_name = 'restaurant/menu-item-detail.html'
+    context_object_name = 'menu'
